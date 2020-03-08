7d104b53b3f6bd410843c805a008dd5b1c830381
Mirko Nava <mirko@idsia.ch>
2020-03-08 20:59:18 +0100

    pre organizing commit

--- bnn.py
+++ bnn.py
@@ -24,16 +24,17 @@ class KLDivergence(torch.nn.Module):
 
     def forward(self, model):
         result = 0
+        n_layers = 0
         for layer in model.layers:
-            if isinstance(layer, BayesianLinearNormal):
-                W = layer.weight
-                b = layer.bias
-                prior = layer.prior
+            if hasattr(layer, 'weight') and isinstance(layer.weight, NormalWeight):
+                result += self._kl_normal_normal(layer.prior, layer.weight)
+                n_layers += 1
 
-                result += self._kl_normal_normal(prior, W)
-                result += self._kl_normal_normal(prior, b)
+            if hasattr(layer, 'bias') and isinstance(layer.bias, NormalWeight):
+                result += self._kl_normal_normal(layer.prior, layer.bias)
+                n_layers += 1
 
-        return result / len(model.layers)
+        return result / n_layers
 
 
 class NormalWeight(torch.nn.Module):
@@ -49,14 +50,18 @@ class NormalWeight(torch.nn.Module):
             torch.empty(*channels, requires_grad=True))
         torch.nn.init.normal_(self.scale, -2, 1)
 
-        self.sample()
+        # self.sample()
 
-    def size(self):
-        return self.mean.size()
+    def size(self, *dims):
+        return self.mean.size(*dims)
 
     @property
     def shape(self):
         return self.mean.size()
+
+    @property
+    def device(self):
+        return self.mean.device
 
     @property
     def requires_grad(self):
@@ -88,19 +93,75 @@ class BayesianLinearNormal(torch.nn.Module):
         self.bias = NormalWeight(out_channels)
         self.prior = prior
 
-    @property
-    def sampled(self):
-        return self.weight.sampled, self.bias.sampled
+        self.sample()
 
     def sample(self):
         self.weight.sample()
         self.bias.sample()
+        self.sampled = (self.weight.sampled, self.bias.sampled)
 
     def forward(self, x, sample=True):
         if sample:
             self.sample()
 
         return torch.nn.functional.linear(x, *self.sampled)
+
+
+class FlipoutLinearNormal(torch.nn.Module):
+
+    def __init__(self, in_channels, out_channels, prior=torch.distributions.normal.Normal(0, .1)):
+        super(FlipoutLinearNormal, self).__init__()
+
+        self.weight = NormalWeight(out_channels, in_channels)
+        # self.bias = NormalWeight(out_channels) # todo: not sure if bias is used here
+        self.prior = prior
+
+        self.sample()
+
+    def sample(self):
+        self.R = (torch.rand(self.weight.size(0),
+                             device=self.weight.device) - .5).sign()
+        self.S = (torch.rand(self.weight.size(1),
+                             device=self.weight.device) - .5).sign()
+        self.sampled = (self.R, self.S)
+
+    def forward(self, x, sample=True):
+        if sample:
+            self.sample()
+
+        b_part = (x * self.S).matmul(self.weight.stddev.t()) * self.R
+
+        return torch.nn.functional.linear(x, self.weight.mean, b_part)
+
+
+class FlipoutConv2dNormal(torch.nn.Module):
+
+    def __init__(self, in_channels, out_channels, prior=torch.distributions.normal.Normal(0, .1)):
+        super(FlipoutConv2dNormal, self).__init__()
+
+        self.weight = NormalWeight(out_channels, in_channels)
+        self.bias = NormalWeight(out_channels)
+        self.prior = prior
+
+        self.sample()
+
+    def sample(self):
+        self.R = (torch.rand(self.weight.size(0),
+                             device=self.weight.device) - .5).sign()
+        self.S = (torch.rand(self.weight.size(1),
+                             device=self.weight.device) - .5).sign()
+        self.sampled = (self.R, self.S)
+
+    def forward(self, x, sample=True):
+        if sample:
+            self.sample()
+
+        output = torch.nn.functional.conv2d(x, self.weight.mean,
+                                            self.bias.mean)
+        output += torch.nn.functional.conv2d(x * self.S,
+                                             self.weight.stddev) * self.R
+
+        return output
 
 
 class BayesianModelBase(torch.nn.Module):
@@ -170,6 +231,37 @@ class BayesianConvolutionalNeuralNetwork(BayesianModelBase):
         return [self.layers(x) for _ in range(samples)]
 
 
+class FlipoutConvolutionalNeuralNetwork(BayesianModelBase):
+
+    def __init__(self, in_channels, out_channels,
+                 prior=torch.distributions.normal.Normal(0, .1), samples=30):
+        super(FlipoutConvolutionalNeuralNetwork, self).__init__(
+            in_channels, out_channels, prior, samples
+        )
+
+        self.layers = torch.nn.Sequential(
+            torch.nn.Conv2d(in_channels, 8, 3),
+            torch.nn.ReLU(),
+            torch.nn.MaxPool2d(2),
+            torch.nn.Conv2d(8, 8, 3),
+            torch.nn.ReLU(),
+            Flatten(),
+            FlipoutLinearNormal(968, 512, prior=prior),
+            torch.nn.ReLU(),
+            FlipoutLinearNormal(512, out_channels, prior=prior),
+            torch.nn.Softmax(dim=-1)
+        )
+
+    def summary(self, *args, **kwargs):
+        summary(self, *args, **kwargs)
+
+    def forward(self, x, samples=None):
+        if samples is None:
+            samples = self.samples
+
+        return [self.layers(x) for _ in range(samples)]
+
+
 class PruneBayesianNormal():
 
     def _prune_linear_normal(self, weight, percentage):
@@ -183,7 +275,7 @@ class PruneBayesianNormal():
 
     def prune(self, model, percentage=0.5):
         for layer in model.layers:
-            if type(layer) == BayesianLinearNormal:
+            if hasattr(layer, 'weight') and isinstance(layer.weight, NormalWeight):
                 self._prune_linear_normal(layer.weight,
                                           percentage)
 
